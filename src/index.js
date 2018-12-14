7d9af9c47637bb9ce8a1265c4a6ef15b31615a0c
Victor Mignot <victor.mgnt@protonmail.com>
2018-12-14 17:23:35 +0100

    manage teams and channels

--- src/index.js
+++ src/index.js
@@ -1,32 +1,169 @@
-const http = require('http');
-const StreamReader = require('./StreamReader')
+const StreamReader = require('./services/StreamReader')
+const express = require('express')
+const bodyParser = require('body-parser')
 
-let clients = [];
+console.log('# Welcome on RadioBretzel ! #')
 
-const stream = new StreamReader('https://s2.radio.co/sdf9aeb4e9/listen')
+var app = express()
 
-stream.startReading()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.listen(8001)
+
+console.log('Server running on port 8001')
 
 
-// When a chunk of data is received on the stream, push it to all connected clients
-stream.on("data", function (chunk) {
-  if (clients.length > 0) {
-    for (client in clients) {
-      clients[client].write(chunk);
-    };
+let data = {
+  teams: []
+}
+
+// TEAMS
+
+app.get('/teams', function (req, res) {
+  res.setHeader('Content-Type', 'application/json')
+  res.send(JSON.stringify(data.teams))
+})
+
+app.get('/teams/:name', function (req, res) {
+
+  let team = data.teams.find(team => {
+    return team.name === req.params.name
+  })
+
+  res.setHeader('Content-Type', 'application/json')
+  res.send(JSON.stringify(team))
+})
+
+app.post('/teams', function (req, res) {
+
+  let team = {
+    name: req.body.name,
+    channels: []
   }
-});
 
-// Listen on a web port and respond with a chunked response header. 
-var server = http.createServer(function (req, res) {
+  data.teams.push(team)
+
+  console.log('Team added')
+
+  res.status(201).send()
+})
+
+app.put('/teams/:name', function (req, res) {
+  let team = data.teams.find(team => {
+    return team.name === req.params.name
+  })
+
+  team.name = req.body.name
+
+  console.log('Team updated')
+
+  res.status(200).send()
+})
+
+app.delete('/teams/:name', function (req, res) {
+  let teamIndex = data.teams.findIndex(team => {
+    return team.name === req.params.name
+  })
+
+  data.teams.splice(teamIndex, 1)
+  console.log('Team deleted')
+
+  res.status(200).send()
+})
+
+
+// STREAMS
+
+app.get('/teams/:team/channels', function (req, res) {
+
+  let team = data.teams.find(team => {
+    return team.name === req.params.team
+  })
+
+  let channels = team.channels.map(channel => {
+    return channel.name
+  })
+
+  res.setHeader('Content-Type', 'application/json')
+  res.send(JSON.stringify(channels))
+})
+
+app.get('/teams/:team/channels/:name', function (req, res) {
+
+  let team = data.teams.find(team => {
+    return team.name === req.params.team
+  })
+
+  let channel = team.channels.find(channel => {
+    return channel.name === req.params.name
+  })
+
   res.writeHead(200, {
     "Content-Type": "audio/mpeg",
     'Transfer-Encoding': 'chunked'
-  });
-  // Add the response to the clients array to receive streaming
-  clients.push(res);
-  console.log('Client connected; streaming');
-});
-server.listen("8000", "127.0.0.1");
+  })
 
-console.log('Server running at http://127.0.0.1:8000'); 
+  channel.clients.push(res)
+  console.log('Client connected to channel ' + channel.name)
+})
+
+app.post('/teams/:team/channels', function (req, res) {
+  let team = data.teams.find(team => {
+    return team.name === req.params.team
+  })
+
+  let channel = {
+    name: req.body.name,
+    source: req.body.source,
+    stream: new StreamReader(req.body.source),
+    clients: []
+  }
+  
+  channel.stream.startReading()
+  
+  channel.stream.on('data', function (chunk) {
+    if (channel.clients.length > 0) {
+      for (client in channel.clients) {
+        channel.clients[client].write(chunk);
+      }
+    }
+  })
+
+  team.channels.push(channel)
+  
+  console.log('Channel added')
+
+  res.status(201).send()
+})
+
+app.put('/teams/:team/channels/:name', function (req, res) {
+  let team = data.teams.find(team => {
+    return team.name === req.params.team
+  })
+
+  let channel = team.channels.find(channel => {
+    return channel.name === req.params.name
+  })
+
+  channel.name = req.body.name
+
+  console.log('Channel updated')
+
+  res.status(200).send()
+})
+
+app.delete('/teams/:team/channels/:name', function (req, res) {
+  let team = data.teams.find(team => {
+    return team.name === req.params.team
+  })
+
+  let channelIndex = team.channels.findIndex(channel => {
+    return channel.name === req.params.name
+  })
+
+  team.channels.splice(channelIndex, 1)
+  console.log('Channel deleted')
+
+  res.status(200).send()
+})
